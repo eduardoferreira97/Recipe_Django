d938b3106c82838203c919c3a4c55f3f5855a3ea
eduardoferreira97 <animedudu12345@gmail.com>
2022-12-16 14:33:39 -0300

    Detail page and filter tag created

--- cook/urls.py
+++ cook/urls.py
@@ -7,6 +7,7 @@ app_name = 'cook'
 
 urlpatterns = [
     path('',views.index, name="index"),
-    path('recipes/category/<int:category_id>/',views.category, name="category")
+    path('recipes/category/<int:pk>/',views.category, name="category"),
+    path('recipes/<int:pk>',views.details, name="details"),
     # path('', include(recipe_api_v2_router.urls)),
 ]
--- cook/views.py
+++ cook/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_list_or_404, get_object_or_404
 
 from .models import Recipe
 
 def index(request):
-    recipes = Recipe.objects.order_by('title')
-    return render(request,"cook/home.html",{'recipe': recipes})
+
+    recipes = Recipe.objects.filter(is_published=True).order_by('title')
+    return render(request,"cook/home.html",{'recipes': recipes, 'title':f'Home | Recipes'})
     
-def category(request,category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
-    return render(request,"cook/home.html",context = {'recipes':recipes})
+def category(request,pk):
+
+    # recipes = Recipe.objects.filter(category__id=pk,is_published=True).order_by('-id')
+    recipes = get_list_or_404(Recipe.objects.filter(category__id=pk,is_published=True).order_by('-id'))
+
+    return render(request,"cook/home.html",context = {'recipes':recipes, 'title':f'{recipes[0].category.name} | Category'})
+
+def details(request,pk):
+
+    recipes = get_object_or_404(Recipe,pk=pk)
+
+    return render(request,"cook/recipe_details.html",context = {'recipe':recipes, 'title':f'{recipes.title}','is_detail_page': True})
